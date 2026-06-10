ec0a78e90cafd64ad0fc33d2d1065f47c21f813c
Manu Sinan <manusinan@Manus-MacBook-Air.local>
2026-06-10 12:53:31 +0530

    Migrate compiled React Three Fiber bundle to unbundled source code and upgrade to React 19 / Vite 7

--- src/components/react-dom-alias.js
+++
@@ -1,9 +0,0 @@
-import { c as ReactDOMInstance } from './index-DV-1WFZA.js';
-
-export default ReactDOMInstance;
-export const {
-  createRoot,
-  hydrateRoot,
-  findDOMNode,
-  version,
-} = ReactDOMInstance;
--- src/components/react-jsx-runtime-alias.js
+++
@@ -1,6 +0,0 @@
-import { r as ReactInstance, J as jsxRuntime } from './index-DV-1WFZA.js';
-
-export const jsx = jsxRuntime.jsx;
-export const jsxs = jsxRuntime.jsxs;
-export const jsxDEV = jsxRuntime.jsxDEV || jsxRuntime.jsx;
-export const Fragment = ReactInstance.Fragment;
